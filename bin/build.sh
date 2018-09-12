20ba42c126b9b696b4c44573d3b36d6d96f92cd5
Bharath Vadlamannati <bharry29@gmail.com>
2018-09-12 01:06:28 -0700

    Updated theme and layout

--- bin/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wkhtmltopdf \
-  --user-style-sheet fonts/print.css \
-  --print-media-type \
-  --page-size A4 \
-  -T 12mm -R 10mm -B 0mm -L 14mm \
-  index.html CV_Bharath_V.pdf
